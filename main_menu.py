87c37a7fbe04159e8cc04f1a6c2854a32cfb60aa
Linda Choucha <mehdi.choucha@laplateforme.io>
2025-03-21 10:06:16 +0100

    :sparkles: add links between the menus

--- main_menu.py
+++ main_menu.py
@@ -13,29 +13,45 @@ window.title("Menu Principal ")
 window.resizable(False, False)
 window.geometry("800x600") 
 
-def deposit():
+def logoff():
         messagebox.showinfo("Déconnexion", "Au Revoir")
         window.destroy()
+        import login_menu
         print("Déconnecté")
+
+def deposit():
+        window.destroy()
+        import deposit_menu
+        print("Dépôt")
+
+def withdrawal():
+        window.destroy()
+        import withdrawal_menu
+        print("Retrait")
 
 def history():
         window.destroy()
         import history_menu
-        print("Déconnecté")
+        print("Historique")
 
-deposit_button = customtkinter.CTkButton(window, text = "Dépôt", height = 70, width = 150, cursor = "hand2", command = history)
+def graph():
+        window.destroy()
+        import graph_menu
+        print("Graphiques")
+
+deposit_button = customtkinter.CTkButton(window, text = "Dépôt", height = 70, width = 150, cursor = "hand2", command = deposit)
 deposit_button.place(rely=0.4,relx=0.25)
 
-withdrawal_button = customtkinter.CTkButton(window, text = "Retrait", height = 70, width = 150, cursor = "hand2", command = history)
+withdrawal_button = customtkinter.CTkButton(window, text = "Retrait", height = 70, width = 150, cursor = "hand2", command = withdrawal)
 withdrawal_button.place(rely=0.4,relx=0.5)
 
 history_button = customtkinter.CTkButton(window, text = "Transactions",  height = 70, width = 150, cursor = "hand2", command = history)
 history_button.place(rely=0.6,relx=0.25)
 
-graph_button = customtkinter.CTkButton(window, text = "Graphiques", height = 70, width = 150, cursor = "hand2", command = history)
+graph_button = customtkinter.CTkButton(window, text = "Graphiques", height = 70, width = 150, cursor = "hand2", command = graph)
 graph_button.place(rely=0.6,relx=0.5)
 
-logoff_button = customtkinter.CTkButton(window, text = "Déconnexion", height = 50, width = 130, cursor = "hand2", command = deposit)
+logoff_button = customtkinter.CTkButton(window, text = "Déconnexion", height = 50, width = 130, cursor = "hand2", command = logoff)
 logoff_button.place(rely=0.8,relx=0.2)
 
 window.mainloop()
